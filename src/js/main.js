578dc0c6dc90396bf9343a1b28c74df98961767f
Ильдар Мустафин <creat4great@gmail.com>
2024-06-10 11:25:54 +0300

    Полуфабрикат ЛК

--- src/js/main.js
+++ src/js/main.js
@@ -30,10 +30,11 @@ window.addEventListener("DOMContentLoaded", () => {
   }
   const header = document.querySelector('header');
   const navToggler = header.querySelector('.nav-toggler');
-  const navTogglerText = navToggler.querySelector('.nav-toggler__text');
-  const headerContent = header.querySelector('.header-content');
-
+  
   if (navToggler) {
+
+    const navTogglerText = navToggler.querySelector('.nav-toggler__text');
+    const headerContent = header.querySelector('.header-content');
     navToggler.onclick = () => {
         header.classList.toggle('active');
         headerContent.classList.toggle('bg-white');
@@ -59,3 +60,206 @@ if (probability) {
     probabilityScale.setAttribute('style', 'width: '+ probabilityValue + '%')
     
 }
+
+// Таймер отправки смс-кода
+
+const smsTimer = document.getElementById('js-timer');
+const updateTimerBtn = document.getElementById('js-update-timer');
+let timerCount = 10;
+let timerInterval;
+
+function updateTimer() {
+    if (timerCount >= 0) {
+        const secondsText = getSecondsText(timerCount);
+        smsTimer.textContent = timerCount < 10 ? '0' + timerCount  + ' ' + secondsText : timerCount  + ' ' + secondsText;
+        timerCount--;
+    } else {
+        smsTimer.style.display = 'none';
+        updateTimerBtn.classList.remove('text-gray_text', 'pointer-events-none');
+        clearInterval(timerInterval);
+    }
+}
+
+if (smsTimer) {
+    timerInterval = setInterval(updateTimer, 1000);
+
+    function sendSms() {
+        timerCount = 10;
+        timerInterval = setInterval(updateTimer, 1000);
+        smsTimer.style.display = 'block';
+        updateTimerBtn.classList.add('text-gray_text', 'pointer-events-none');
+    }
+}
+
+// Функция определения правильного окончания для слова "секунда"
+function getSecondsText(timerCount) {
+    if (timerCount === 1 || (timerCount > 20 && timerCount % 10 === 1)) {
+        return 'секунда';
+    } else if ((timerCount >= 2 && timerCount <= 4) || (timerCount > 20 && timerCount % 10 >= 2 && timerCount % 10 <= 4)) {
+        return 'секунды';
+    } else {
+        return 'секунд';
+    }
+}
+
+// Показ/скрытие поля password
+
+const passwordField = document.querySelector('.show-password');
+
+if (passwordField) {
+    let formInputs = document.querySelectorAll('.js-password-wrapper');
+
+    formInputs.forEach(function(formInput) {
+        var showPassword = formInput.querySelector('.show-password'); 
+        var input = formInput.querySelector('input[type="password"]');
+
+        if (showPassword) {
+            showPassword.addEventListener('click', function() {
+                if (input.type === 'password') {
+                    input.type = 'text';
+                    showPassword.classList.remove('opacity-60');
+                } else {
+                    input.type = 'password';
+                    showPassword.classList.add('opacity-60');
+                }
+            });
+        }
+        
+    });
+}
+
+// активация кнопки при выборе чекбокса
+
+const rulesCheckbox = document.getElementById('rules');
+const nextStepButton = document.querySelector('.js-button');
+
+
+if (rulesCheckbox && nextStepButton) {
+    function checkRulesCheckbox() {
+        if (rulesCheckbox.checked) {
+            nextStepButton.classList.remove('btn-disabled');
+        } else {
+            nextStepButton.classList.add('btn-disabled');
+        }
+    }
+
+    rulesCheckbox.addEventListener('change', checkRulesCheckbox);
+
+    checkRulesCheckbox();
+}
+
+// Показ дополнительных опций в калькуляторе на лендинге
+
+const rulesShow = document.querySelector('.js-show-rules');
+const rulesHide = document.querySelector('.js-hide-rules');
+const hiddenCheckbox = document.querySelector('.js-hidden-checkbox');
+
+if (rulesShow) {
+    rulesShow.addEventListener('click', function () {
+        hiddenCheckbox.classList.remove('hidden')
+    });
+    rulesHide.addEventListener('click', function () {
+        hiddenCheckbox.classList.add('hidden')
+    })
+}
+
+
+// Меню в личном кабинете
+const accountMenuBtns = document.querySelectorAll('.btn-account-menu');
+const accountMenu = document.querySelector('.js-account-menu');
+if(accountMenuBtns) {
+
+    accountMenuBtns.forEach( function(btn) {
+        btn.onclick = () => {
+            accountMenu.classList.toggle('translate-y-full');
+            if(!accountMenu.classList.contains('translate-y-full')) {
+                enableBodyScroll();
+            } else {
+                disableBodyScroll();
+            }
+        }
+    })
+    
+}
+
+// кастомный селект (ЛК и flow)
+
+const customSelects = document.querySelectorAll('.custom-select');
+
+if (customSelects) {
+    customSelects.forEach(function(customSelect) {
+        let customSelectToggler = customSelect.querySelector('.custom-select__head');
+        let customSelectResult = customSelect.querySelector('.custom-select__result');
+        let customSelectOptions = customSelect.querySelectorAll('.custom-option');
+        
+        customSelectToggler.addEventListener('click', function() {
+            this.parentNode.classList.toggle('active')
+        })
+
+        customSelectOptions.forEach(function(customSelectOption) {
+            customSelectOption.addEventListener('click', function() {
+                let customValue = customSelectOption.querySelector('.custom-select__value').textContent;
+                customSelectOptions.forEach(function(item) {
+                    item.removeAttribute('selected');
+                })
+                this.setAttribute('selected', '');
+                customSelectResult.textContent = customValue;
+                customSelect.classList.remove('active');
+                customSelectToggler.classList.add('selected');
+            })
+        })
+    })
+}
+
+// Открытие графика платежей
+
+const scheduleButton = document.querySelector('.schedule-btn');
+const scheduleModal = document.querySelector('.schedule-modal');
+const scheduleClose = document.querySelector('.schedule-close');
+
+if(scheduleButton) {
+    scheduleButton.addEventListener('click', function () {
+        scheduleModal.classList.remove('translate-y-full');
+    });
+    scheduleClose.addEventListener('click', function () {
+        scheduleModal.classList.add('translate-y-full');
+    })
+}
+
+
+// Открытие информации о заеме в multiple
+
+const loanCards = document.querySelectorAll('.js-loan-card');
+const infoButton = document.querySelector('.js-show-info');
+const loanInfo = document.querySelector('.js-loan-info');
+
+if(loanCards) {
+
+    loanCards.forEach(function(card) {
+        let infoButton = card.querySelector('.js-show-info');
+        let loanInfo = card.querySelector('.js-loan-info');
+        infoButton.addEventListener('click', function () {
+            loanInfo.classList.toggle('max-h-96');
+        });
+    })
+    
+}
+
+const accountSwiper = new Swiper('.loans-slider', {
+    spaceBetween: 30,
+    // Navigation arrows
+    navigation: {
+        nextEl: '.slider-next',
+        prevEl: '.slider-prev',
+    },
+
+    pagination: {
+        el: '.slider-counter',
+        type: 'fraction',
+        renderFraction: function (currentClass, totalClass) {
+            return '<div class="current-slide ' + currentClass + '"></div>' +
+                ' <div class="line"></div> ' +
+                '<div class=" total-slide ' + totalClass + '"></div>';
+        },
+    },
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -21,25 +21,35 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        white: 'rgba(255, 255, 255, 1)',
-        black: 'rgba(21, 21, 21, 1)',
+        white: '#FFFFFF',
+        black: '#191919',
         black_02: 'rgba(0, 0, 0, 0.2)',
 
-        error: 'rgba(255, 0, 0, 1)',
+        dark_accent: '#D7AB50',
+        accent: '#F1C025',
 
-        dark_accent: 'rgba(82, 17, 36, 1)',
-        accent: 'rgba(205, 39, 89, 1)',
+        gray_text: '#666666',
+        darkgray: '#D9D9D9',
+        gray_015: 'rgba(163, 163, 163, 0.15)',
+        gray: '#EDEDED',
+        lightgray: '#F4F5F7',
 
-        darkgrey: '#A3A3A3',
-        grey_015: 'rgba(163, 163, 163, 0.15)',
-        grey: 'rgba(163, 163, 163, 1)',
-        lightgrey: 'rgba(245, 245, 245, 1)',
+        red: '#DD2525',
+        lightred: '#FF0000',
 
-        darkgreen: '#6BD390',
-        green: 'rgba(107, 211, 144, 1)',
+        green: '#6BD390',
+        lightgreen: '#6BD390',
 
-        orange: '#FCAA75',
+        blue_text: '#0E174A',
+        blue: '#0E174A',
+        darkblue: '#394D69',
+        blue_08: 'rgba(14,23,74,.8)',
+
+        purple: '#312D4B',
       },
+      backgroundImage: {
+        'account-banner': "url('/img/backgrounds/banner-background.svg')",
+      }
     },
   },
   plugins: plugins,
